4fded21844cfc0f81a7851fd3d076c1ab62ca9d3
Ciaran Downey <me@ciarand.me>
2014-07-08 22:37:03 -0700

    Fix 5.3 compatibility because I'm a softy

--- test/RouterTest.php
+++ test/RouterTest.php
@@ -35,138 +35,138 @@ class RouterTest extends TestCase
 
     public function handlerDataProvider()
     {
-        return [
-            [
+        return array(
+            array(
                 "./_assets/inc/global.header.php",
                 "/_assets/inc/global.header.php",
-            ],
-            [
+            ),
+            array(
                 "./_assets/inc/global.scripts.php",
                 "/_assets/inc/global.scripts.php",
-            ],
-            [
+            ),
+            array(
                 "./_assets/inc/mainnav.inc.php",
                 "/_assets/inc/mainnav.inc.php",
-            ],
-            [
+            ),
+            array(
                 "./_assets/inc/mobile-state-retailer-lists.php",
                 "/_assets/inc/mobile-state-retailer-lists.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/check.php",
                 "/vehicle-finder/check.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/cart/index.php",
                 "/vehicle-finder/costco/cart/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/index.php",
                 "/vehicle-finder/costco/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/membership-info/index.php",
                 "/vehicle-finder/costco/membership-info/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/results/index.php",
                 "/vehicle-finder/costco/results/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/save-results/index.php",
                 "/vehicle-finder/costco/save-results/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/template.php",
                 "/vehicle-finder/costco/template.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/vehicle-finder/getdata.php",
                 "/vehicle-finder/costco/vehicle-finder/getdata.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/costco/vehicle-finder/index.php",
                 "/vehicle-finder/costco/vehicle-finder/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/dbconfig.php",
                 "/vehicle-finder/dbconfig.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/dbtest.php",
                 "/vehicle-finder/dbtest.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/getdata.php",
                 "/vehicle-finder/getdata.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/index.php",
                 "/vehicle-finder/index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/index2.php",
                 "/vehicle-finder/index2.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/OLD_index.php",
                 "/vehicle-finder/OLD_index.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/results.php",
                 "/vehicle-finder/results.php",
-            ],
-            [
+            ),
+            array(
                 "./vehicle-finder/Retriever.php",
                 "/vehicle-finder/Retriever.php",
-            ],
-            [
+            ),
+            array(
                 "./vendor/db/DbConnect.php",
                 "/vendor/db/DbConnect.php",
-            ],
-            [
+            ),
+            array(
                 "./vendor/db/helpers.php",
                 "/vendor/db/helpers.php",
-            ],
-            [
+            ),
+            array(
                 "./vendor/db/import.php",
                 "/vendor/db/import.php",
-            ],
-            [
+            ),
+            array(
                 "vendor/db/import_ref.php",
                 "/vendor/db/import_ref.php",
-            ],
-            [
+            ),
+            array(
                 "vendor/db/positions.php",
                 "/vendor/db/positions.php",
-            ],
-            [
+            ),
+            array(
                 "vendor/db/RowObject.php",
                 "/vendor/db/RowObject.php",
-            ],
-        ];
+            ),
+        );
     }
 
     public function securityDataProvider()
     {
-        return [
-            ["../../malicious_script.php", "/malicious_script.php"],
-            ["/subfolder/../../../malicious_script.php", "/subfolder/malicious_script.php"],
-        ];
+        return array(
+            array("../../malicious_script.php", "/malicious_script.php"),
+            array("/subfolder/../../../malicious_script.php", "/subfolder/malicious_script.php"),
+        );
     }
 
     public function indexFileDataProvider()
     {
-        return [
-            ["/subdir/", "/subdir/index.php"],
-            ["/subdir", "/subdir/index.php"],
-            ["/", "/index.php"],
-        ];
+        return array(
+            array("/subdir/", "/subdir/index.php"),
+            array("/subdir", "/subdir/index.php"),
+            array("/", "/index.php"),
+        );
     }
 
     private function compareResultWithExpected($url, $expected)
     {
-        $env = ["SCRIPT_NAME" => $url];
+        $env = array("SCRIPT_NAME" => $url);
 
         $this->assertSame($expected, Router::scriptForEnv($env));
     }
